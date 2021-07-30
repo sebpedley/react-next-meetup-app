57113acba24a4b27f7ba9df4bcde7ac69de83405
sebpedley <pedleyseb@gmail.com>
2021-07-30 15:46:39 +0100

    Adding some 'head' meta data for SEO

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,19 @@
+import Head from 'next/head';
 import { MongoClient } from 'mongodb';
 
 import MeetupList from '../components/meetups/MeetupList';
+import { Fragment } from 'react';
 
 function HomePage(props) {
-    return <MeetupList meetups={props.meetups} />
+    return (
+        <Fragment>
+            <Head>
+                <title>React Meetups</title>
+                <meta name="description" content="Browse a huge list of highly active React meetups!" />
+            </Head>
+            <MeetupList meetups={props.meetups} />
+        </Fragment>
+    );
 }
 
 // This will always run on the server after deployment (not client's computer)
--- pages/new-meetup/index.js
+++ pages/new-meetup/index.js
@@ -1,4 +1,6 @@
+import Head from 'next/head';
 import { useRouter } from 'next/router';
+import { Fragment } from 'react';
 import NewMeeupForm from '../../components/meetups/NewMeetupForm';
 
 function NewMeetupPage() {
@@ -16,12 +18,18 @@ function NewMeetupPage() {
 
         const data = await response.json();
 
-        console.log(data);
-
         router.push('/');
     }
 
-    return <NewMeeupForm onAddMeetup={addMeetupHandler} />
+    return (
+        <Fragment>
+            <Head>
+                <title>Add a New Meetup</title>
+                <meta name="description" content="Add your own meetups and create amazing networking opportunities." />
+            </Head>
+            <NewMeeupForm onAddMeetup={addMeetupHandler} />
+        </Fragment>
+    );
 }
 
 export default NewMeetupPage;
